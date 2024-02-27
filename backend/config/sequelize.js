020b51fa623be81fdbb7ebf8121237adb6cd1fb9
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2024-02-27 17:49:38 +0900

    fix(sequelize): config added port

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.POSTGRES_PASSWORD,
   {
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: "postgres",
   },
 );
