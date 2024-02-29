76e3f04864edb5e1759d5fcce807f130e6252daa
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2024-02-29 15:54:13 +0900

    feat(meta): added meta data, favicon title etc...

--- custom-higher-or-lower/app/layout.tsx
+++ custom-higher-or-lower/app/layout.tsx
@@ -19,8 +19,49 @@ export default function RootLayout({
   return (
     <html>
       <head>
-        <title>HigherOrLower</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Higher or lower game</title>
+        <meta
+          name="description"
+          content="Play the higher or lower game with any category you want !"
+        />
+        <meta
+          name="keywords"
+          content="higher or lower, game, higher, lower, guess, category, categories"
+        />
+        <meta name="author" content="Ahmed" />
+        <meta property="og:title" content="Higher or lower game" />
+        <meta
+          property="og:description"
+          content="Play the higher or lower game with any category you want !"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_SELF_URL} />
+        <meta property="og:image" content="/cover_image.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>
         <Provider store={store}>
